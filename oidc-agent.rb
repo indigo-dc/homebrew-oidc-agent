dfdff7cbd5453ac31959e5a633f2aaa85223966a
zachmann <gabriel.zachmann@kit.edu>
2019-05-28 14:45:13 +0200

    fix hash value

--- oidc-agent.rb
+++ oidc-agent.rb
@@ -6,7 +6,7 @@ class OidcAgent < Formula
   homepage "https://github.com/indigo-dc/oidc-agent/"
   url "https://api.github.com/repos/indigo-dc/oidc-agent/tarball/v3.1.0"
   version "3.1.0"
-  sha256 "907e72eb8c1a19d29d66c8c3168b16d81238717d7737581fe053ef7a8002aff3  -"
+  sha256 "907e72eb8c1a19d29d66c8c3168b16d81238717d7737581fe053ef7a8002aff3"
 
   depends_on "pkg-config"
   depends_on "argp-standalone"
