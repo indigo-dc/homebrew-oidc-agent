f252973655dedd7fa3a84f1e6f1830434ad2c47e
zachmann <gabriel.zachmann@kit.edu>
2020-07-06 15:23:22 +0200

    update update scripts

--- updateToNewestRelease.sh
+++ updateToNewestRelease.sh
@@ -2,15 +2,6 @@
 
 json=$(http https://api.github.com/repos/indigo-dc/oidc-agent/releases/latest)
 tag=$(echo $json | jq -r '.tag_name')
-tar_url=$(echo $json | jq -r '.tarball_url')
-tag=$(echo $json | jq -r '.tag_name')
-version=${tag:1}
-sha256=$(curl -L $tar_url 2>/dev/null| sha256sum | awk '{print $1}')
+version=${tag/#v/}
 
-sed -i "s!url\ \".*\"!url\ \"${tar_url}\"!g" oidc-agent.rb
-sed -i "s/version\ \".*\"/version\ \"${version}\"/g" oidc-agent.rb
-sed -i "s/sha256\ \".*\"/sha256\ \"${sha256}\"/g" oidc-agent.rb
-
-echo "Updated version to: ${version}"
-echo "Updated url to: ${tar_url}"
-echo "Updated sha256 to: ${sha256}"
+$(dirname $0)/updateToRelease.sh $version
---
+++ updateToRelease.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tag="v$1"
+version=${tag:1}
+json=$(http https://api.github.com/repos/indigo-dc/oidc-agent/releases/tags/$tag)
+tar_url=$(echo $json | jq -r '.tarball_url')
+sha256=$(curl -L $tar_url 2>/dev/null| sha256sum | awk '{print $1}')
+
+sed -i "s!url\ \".*\"!url\ \"${tar_url}\"!g" oidc-agent.rb
+sed -i "s/version\ \".*\"/version\ \"${version}\"/g" oidc-agent.rb
+sed -i "s/sha256\ \".*\"/sha256\ \"${sha256}\"/g" oidc-agent.rb
+
+echo "Updated version to: ${version}"
+echo "Updated url to: ${tar_url}"
+echo "Updated sha256 to: ${sha256}"
