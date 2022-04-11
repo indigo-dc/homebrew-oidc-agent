bb2b2a05859cd6252a12e598be41fc6f6a227c19
zachmann <gabriel.zachmann@kit.edu>
2022-04-11 12:26:57 +0200

    update update scripts

--- updateToRelease.sh
+++ updateToRelease.sh
@@ -2,16 +2,20 @@
 
 token=$(git config --global github.token)
 
-tag="v$1"
-version=${tag:1}
-json=$(http https://api.github.com/repos/indigo-dc/oidc-agent/releases/tags/$tag "Authorization: Bearer ${token}")
-tar_url=$(echo $json | jq -r '.tarball_url')
+version=$1
+tag="v$version"
+formula=$2
+
+git checkout -b "$version"
+
+tar_url=$(echo "https://github.com/indigo-dc/oidc-agent/archive/refs/tags/VTAG.tar.gz" | sed "s/VTAG/"${tag}"/g")
 sha256=$(curl -L $tar_url 2>/dev/null| sha256sum | awk '{print $1}')
 
-sed -i "s!url\ \".*\"!url\ \"${tar_url}\"!g" oidc-agent.rb
-sed -i "s/version\ \".*\"/version\ \"${version}\"/g" oidc-agent.rb
-sed -i "s/sha256\ \".*\"/sha256\ \"${sha256}\"/g" oidc-agent.rb
+sed -i "s!^[[:space:]]*url\ \".*\"!  url\ \"${tar_url}\"!g" $formula
+sed -i "s/sha256\ \".*\"/sha256\ \"${sha256}\"/g" $formula
 
-echo "Updated version to: ${version}"
 echo "Updated url to: ${tar_url}"
 echo "Updated sha256 to: ${sha256}"
+
+git add "$formula"
+git commit -m ""${version}""
