c3f795675392ccfa63076b3ea02e48fb212149e0
zachmann <gabriel.zachmann@kit.edu>
2022-04-11 12:02:41 +0200

    update update script

--- updateToRelease.sh
+++ updateToRelease.sh
@@ -6,6 +6,8 @@ version=$1
 tag="v$version"
 formula=$2
 
+git checkout -b "$version"
+
 tar_url=$(echo "https://github.com/indigo-dc/oidc-agent/archive/refs/tags/VTAG.tar.gz" | sed "s/VTAG/"${tag}"/g")
 sha256=$(curl -L $tar_url 2>/dev/null| sha256sum | awk '{print $1}')
 
@@ -14,3 +16,6 @@ sed -i "s/sha256\ \".*\"/sha256\ \"${sha256}\"/g" $formula
 
 echo "Updated url to: ${tar_url}"
 echo "Updated sha256 to: ${sha256}"
+
+git add "$formula"
+git commit -m ""${version}""
