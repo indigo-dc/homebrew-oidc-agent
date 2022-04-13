a0f547ff0b6332299f96ad1c082b2620111a18d2
zachmann <gabriel.zachmann@kit.edu>
2022-04-13 08:22:27 +0200

    oidc-agent: rebuild
    * just change something for new workflow run
    * just change something for new workflow run
    
    Closes #10.
    
    Signed-off-by: Gabriel Zachmann <18414445+zachmann@users.noreply.github.com>

--- Formula/oidc-agent.rb
+++ Formula/oidc-agent.rb
@@ -1,6 +1,6 @@
 class OidcAgent < Formula
   desc "Manage OpenID Connect tokens on the command-line"
-  homepage "https://github.com/indigo-dc/oidc-agent/"
+  homepage "https://github.com/indigo-dc/oidc-agent"
   url "https://github.com/indigo-dc/oidc-agent/archive/refs/tags/v4.2.6.tar.gz"
   sha256 "821f554d947082879cbc7be01a64b8edbbf88058f6b20b8ae38fbfe7d90eaabb"
   license "MIT"
