b06e1c2b41729ab040bf133a7f6e01deb1715163
zachmann <gabriel.zachmann@kit.edu>
2022-07-22 11:27:07 +0200

    4.3.2

--- Formula/oidc-agent.rb
+++ Formula/oidc-agent.rb
@@ -1,8 +1,8 @@
 class OidcAgent < Formula
   desc "Manage OpenID Connect tokens on the command-line"
   homepage "https://github.com/indigo-dc/oidc-agent"
-  url "https://github.com/indigo-dc/oidc-agent/archive/refs/tags/v4.3.1.tar.gz"
-  sha256 "c0bc86c9195d43a02890fcde1eaf3da62a2f669d3bb0ddee2db9a37f329c4732"
+  url "https://github.com/indigo-dc/oidc-agent/archive/refs/tags/v4.3.2.tar.gz"
+  sha256 "8f18afa7a066e7a781d3c239afd183115205edacfd58806906c9f48f24bfe2b0"
   license "MIT"
 
   bottle do
