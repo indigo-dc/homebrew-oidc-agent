e0e0e61b11902fd1588b1d31606b29a0b80cdf14
Gabriel Zachmann <18414445+zachmann@users.noreply.github.com>
2023-01-23 11:27:49 +0000

    oidc-agent: update 4.5.0 bottle.

--- Formula/oidc-agent.rb
+++ Formula/oidc-agent.rb
@@ -6,9 +6,9 @@ class OidcAgent < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/indigo-dc/homebrew-oidc-agent/releases/download/oidc-agent-4.4.3"
-    sha256 monterey: "3aad6775b360c6da81b48e0dbd244a5ee352328d1bd2d949a16e68948d1ca598"
-    sha256 big_sur:  "a58f1e663ab6db18898bbe8cfb05ba8cdcd86f59d9f0e4946f9e2a5fd3537731"
+    root_url "https://github.com/indigo-dc/homebrew-oidc-agent/releases/download/oidc-agent-4.5.0"
+    sha256 monterey: "32ccc4286486559488224169ec2bb67feb7bd0e564db1db134dc84475b2cdce2"
+    sha256 big_sur:  "b81fb24d59ea0a47fecd2293c50be0839d5c4fb6f91bbda1ed090f0d47c972a5"
   end
 
   depends_on "help2man" => :build
