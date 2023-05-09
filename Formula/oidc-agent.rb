d806c766749bf585c90c8b7e877c845c36bb8ea4
zachmann <gabriel.zachmann@kit.edu>
2023-05-09 08:36:33 +0200

    add version

--- Formula/oidc-agent.rb
+++ Formula/oidc-agent.rb
@@ -3,6 +3,7 @@ class OidcAgent < Formula
   homepage "https://github.com/indigo-dc/oidc-agent"
   url "https://github.com/indigo-dc/oidc-agent/archive/refs/heads/prerel.tar.gz"
   sha256 "b3b48ecfd0366b7ff1745ed071a05ac0d68f1dc100ef720961571f71653b560d"
+  version "4.5.2"
   license "MIT"
 
   bottle do
