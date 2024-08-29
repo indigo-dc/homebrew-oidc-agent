34a2812854acb377fb9545637a4162c1fbf3b295
Gabriel Zachmann <18414445+zachmann@users.noreply.github.com>
2024-08-29 09:52:45 +0000

    oidc-agent: update 5.2.0 bottle.

--- Formula/oidc-agent.rb
+++ Formula/oidc-agent.rb
@@ -6,10 +6,10 @@ class OidcAgent < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/indigo-dc/homebrew-oidc-agent/releases/download/oidc-agent-5.0.1"
-    sha256 ventura:  "74bf719b0c14d60ef765dc9749249ab5fa14b934a4a775fe90c5eb6a379de8de"
-    sha256 monterey: "780eb334d01d08e8eeb7797cce08755db8529e1e19e9f9290cfbab1a0225168d"
-    sha256 big_sur:  "1f8c1d905eb70db86a3d019b936fb62224ba9e154245cd3ded5bb50291d50c17"
+    root_url "https://github.com/indigo-dc/homebrew-oidc-agent/releases/download/oidc-agent-5.2.0"
+    sha256 arm64_sonoma: "61bf2bc7f8dca1ff8dd4fb05c7a062d23dd0ba844b9a9597d97ab5d74d0c8e80"
+    sha256 ventura:      "c58d4764b3d2fe3d0c8e67928fe9267767da3502863fbddcf822e869b205ba41"
+    sha256 monterey:     "4670f6733f20ce7735802811268fa3f373ca356375cd29c8b5a28a6f880f379a"
   end
 
   depends_on "help2man" => :build
